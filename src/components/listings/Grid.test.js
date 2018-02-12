60a3cb3ee304b730967ab75d8f3ba1c7967917e7
mplibunao <mark.libunao@shoresuite.com>
2018-02-13 06:12:09 +0800

    Added one more string matcher test

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,11 +1,25 @@
 import data from '../../data/courses.json';
 
+// Numbers
 const numItems = data.length;
 
 test('Number of items = 12', () => {
-    expect(numItems).toBe(12);
+  expect(numItems).toBe(12);
 });
 
 test('Number of items to be greater than 12', () => {
-    expect(numItems).toBeGreaterThanOrEqual(12);
+  expect(numItems).toBeGreaterThanOrEqual(12);
 });
+
+// Strings
+const dataTest = data[0].title;
+
+test('There is a JS in this title', () => {
+  expect(dataTest).toMatch(/JS/);
+});
+
+test('The title contains React', () => {
+  expect(dataTest).toContain('React');
+});
+
+// Arrays
